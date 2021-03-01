19f0963f8b6af64c59557372766f517a2294ecf0
mostafa hassan <mostafahassan999888@gmail.com>
2021-03-01 05:16:41 +0200

    Added isAdmin middleware

---
+++ app/Http/Middleware/isAdmin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class isAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if($request->user()->user_type == "admin")
+            return $next($request);
+        else redirect('/') //should add flush message with unauthorized action 
+    }
+}
