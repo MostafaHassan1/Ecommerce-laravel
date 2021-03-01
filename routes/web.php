c03e0bdaa29a0efc10e034c7b1f05f560005b143
mostafa hassan <mostafahassan999888@gmail.com>
2021-03-01 02:02:21 +0200

    installed jetstream

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,6 @@ Route::get('/', HomeComponent::class);
 Route::get('/shop', ShopComponent::class);
 Route::get('/cart', CartComponent::class);
 Route::get('/checkout', CheckoutComponent::class);
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
