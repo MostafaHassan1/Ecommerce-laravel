75e89c98877cc6a4678abb1013f95c4d5c419400
mostafa hassan <mostafahassan999888@gmail.com>
2021-03-19 22:47:56 +0200

    Added laravel mix functionality

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,11 +11,37 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
-        require('tailwindcss'),
-        require('autoprefixer'),
+mix.js("resources/js/app.js", "public/js")
+    .scripts(
+        [
+            "resources/js/jquery-1.12.4.minb8ff.js",
+            "resources/js/jquery-ui-1.12.4.minb8ff.js",
+            "resources/js/bootstrap.min.js",
+            "resources/js/jquery.flexslider.js",
+            "resources/js/owl.carousel.min.js",
+            "resources/js/jquery.countdown.min.js",
+            "resources/js/jquery.sticky.js",
+            "resources/js/functions.js",
+        ],
+        "public/js/all.js"
+    )
+    .styles(
+        [
+            "resources/css/animate.css",
+            "resources/css/font-awesome.min.css",
+            "resources/css/bootstrap.min.css",
+            "resources/css/owl.carousel.min.css",
+            "resources/css/flexslider.css",
+            "resources/css/chosen.min.css",
+            "resources/css/style.css",
+            "resources/css/color-02.css",
+        ],
+        "public/css/all.css"
+    )
+    .postCss("resources/css/app.css", "public/css", [
+        require("postcss-import"),
+        require("tailwindcss"),
+        require("autoprefixer"),
     ]);
 
 if (mix.inProduction()) {
