498cd0bad0c59f85057856260e26a83440e0ac31
mostafa hassan <mostafahassan999888@gmail.com>
2021-04-17 03:25:24 +0200

    WD : Added the slider to the home page

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\HomeSlider;
 use Livewire\Component;
 
 class HomeComponent extends Component
 {
     public function render()
     {
-        return view('livewire.home-component')->layout('layouts.base');
+        return view('livewire.home-component', ['sliders' => HomeSlider::whereStatus('1')->get()])->layout('layouts.base');
     }
 }
